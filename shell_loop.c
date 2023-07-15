c4e6bc5abaf0d076af6b0033ef116f4ce204d881
russiantech <124055269+russiantech@users.noreply.github.com>
2023-07-15 14:35:51 +0100

    loops through the shell/path, find/lloks for commands in path etc

--- shell_loop.c
+++ shell_loop.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 
 /**
- * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * hsh - shell loop
+ * @info: param/return info struct
+ * @av: arg vector from main()
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 , (1 or error code) if errored
  */
+
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -44,14 +45,13 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
+ * find_builtin - looks builtin command
+ * @info: param/return info struct
  *
- * Return: -1 if builtin not found,
- * 0 if builtin executed successfully,
- * 1 if builtin found but not successful,
- * 2 if builtin signals exit()
+ * Return: -1 if not found, 0 if found with success,
+ * 1 if found without successf, 2 if signal exit()
  */
+
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
@@ -78,11 +78,12 @@ int find_builtin(info_t *info)
 }
 
 /**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
+ * find_cmd - looks for command in path
+ * @info: param/return struct info
  *
  * Return: void
  */
+
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
@@ -120,11 +121,12 @@ void find_cmd(info_t *info)
 }
 
 /**
- * fork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
+ * fork_cmd - forks exec thread to run cmd
+ * @info: param/return struct info
  *
- * Return: void
+ * Return: (void)
  */
+
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
