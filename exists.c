ed24bad2b5c3e0f26698dc60ad8ec375143ab570
russiantech <124055269+russiantech@users.noreply.github.com>
2023-07-15 13:11:54 +0100

    operations on strings checking if exists, parsing of strings etc. filename is exists.c

--- exists.c
+++ exists.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ **_strncpy - copy str
+ *@dest: copy str from here
+ *@src: to here
+ *@n: num of char to copy
+ *Return: concatenated str
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
@@ -31,12 +32,13 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ **_strncat - joins 2 str
+ *@dest: 1st str
+ *@src: 2nd str
+ *@n: no of bytes to use
+ *Return: joined str
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
@@ -58,11 +60,12 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **_strchr - find char in str
+ *@s: str in question
+ *@c: char to find
+ *Return: (s) pointer as memory of s
  */
+
 char *_strchr(char *s, char c)
 {
 	do {
